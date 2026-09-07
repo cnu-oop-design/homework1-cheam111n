fda9f219979c0fc0e33ee8208fbc0327ae51d5e6
cheam111n <chemin2413@gmail.com>
2026-09-07 11:11:16 +0900

    202502615 황채민

--- main.cpp
+++ main.cpp
@@ -2,14 +2,12 @@
 
 using namespace std;
 
-// 학번:
-// 이름:
+// 학번: 202502615
+// 이름: 황채민
 
 int main(int argc, const char *argv[]) {
-    // TODO: 아래 두 줄을 정확히 출력하도록 코드를 작성하세요.
-    //
-    //   Hello, OOP!
-    //   C++ is ready.
+    cout << "Hello, OOP!" << endl;
+    cout << "C++ is ready." << endl;
 
     return 0;
 }
